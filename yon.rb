797c5b8c121ecbf4aaee4a6772d7849fb82a71fc
Emam Hasan <emam.git@gmail.com>
2026-05-13 22:01:22 +0600

    updated checksum

--- yon.rb
+++ yon.rb
@@ -5,19 +5,19 @@ class Yon < Formula
 
   if OS.mac? && Hardware::CPU.arm?
     url "https://github.com/emam-h/yon/releases/download/v0.1.0/yon-darwin-arm64.tar.gz"
-    sha256 "3c7f79f4fb188cb2205d21ca62bf428c599692f655946c2e5d13657db56e278d"
+    sha256 "64d4df9dfd0b2c417453d9d40737c947c795625445bf8b8bcb46ca28c287e8b4"
 
   elsif OS.mac? && Hardware::CPU.intel?
     url "https://github.com/emam-h/yon/releases/download/v0.1.0/yon-darwin-amd64.tar.gz"
-    sha256 "f0112667aeb34a4dc4402fc8a75b41833727d5169bb7593f56f2a52d92a6f662"
+    sha256 "4f0ae83d0a97843d895e27c9b4594026785424703b3d458f810ad192a3694e8c"
 
   elsif OS.linux? && Hardware::CPU.arm?
     url "https://github.com/emam-h/yon/releases/download/v0.1.0/yon-linux-arm64.tar.gz"
-    sha256 "8e1e20c3f89121a6f3eb430bf4c85452d472fbd5137548972f6fccd5677ae7b4"
+    sha256 "cc7c495707644334ff6a460ce699b747e8db49140291b8eaaa709baa0c1453d9"
 
   elsif OS.linux? && Hardware::CPU.intel?
     url "https://github.com/emam-h/yon/releases/download/v0.1.0/yon-linux-amd64.tar.gz"
-    sha256 "239b3fc82a69989fb90ca581f39ad0d7390306a0f8ce649d38263f80d01c1696"
+    sha256 "04ea62dce4ca96e24264ddd918e8c9546a121bc0a4e2223ba3dafe759ac845a6"
 
   else
     odie "Unsupported platform"
